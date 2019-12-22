e66cf05caf53f1c5b87e8f2bfe19174f412cdcbe
mustafagokces <facemusti@gmail.com>
2019-12-23 00:00:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+    <h1 class="header">MUSTAFA GÖKÇE</h1>
+      <p class="paragraf">ELECTRİC AND ELECTRONİC ENGİNEERİNG | GAUN</p>
+      <p class="mustafa">I am a student as a engineer.I am from america and I live in Canada</p>
+    <hr class="divider02" />
+    <div class="header">
+      <a href="https://www.facebook.com/facemusti?ref=bookmarks" class="fa fa-facebook"></a>
+      <a href="https://twitter.com/mustafagokces" class="fa fa-twitter"></a>
+      <a href="https://www.linkedin.com/in/mustafa-g%C3%B6k%C3%A7e-0031a0196/" class="fa fa-linkedin"></a>
+      <a href="https://www.youtube.com/channel/UCRFyKXhOv08Is3RcPzvIhXQ?view_as=subscriber" class="fa fa-youtube"></a>
+      <a href="https://www.instagram.com/mustafagokces/?hl=tr" class="fa fa-instagram"></a>
+    <hr class="divider03" />
+    <h5 class="icon-7 normal">
+      <code>
+        <a class="mail" href="mailto:2mustafagokce@gmai.com">
+            E-mail-> 2mustafagokce@gmail.com
+        </a>
+      </code>
+      </h5>
+    </div>
+    </div>
+  );
+}
+
+export default App;
