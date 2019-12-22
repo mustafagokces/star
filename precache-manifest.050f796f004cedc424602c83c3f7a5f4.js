dce9569243bbd8da39cd1f1ca654eb84b08a2200
mustafagokces <facemusti@gmail.com>
2019-12-23 00:04:00 +0300

    Updates

---
+++ precache-manifest.050f796f004cedc424602c83c3f7a5f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab763df5767da5a1f3d65cba24a08af",
+    "url": "/star/index.html"
+  },
+  {
+    "revision": "38aef2c8dc679fb9d76c",
+    "url": "/star/static/css/main.64e57354.chunk.css"
+  },
+  {
+    "revision": "c28a108f93354ba7bb9a",
+    "url": "/star/static/js/2.4bd8a724.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star/static/js/2.4bd8a724.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38aef2c8dc679fb9d76c",
+    "url": "/star/static/js/main.ad23ddbd.chunk.js"
+  },
+  {
+    "revision": "378344af50a86e18a8a0",
+    "url": "/star/static/js/runtime-main.91f15785.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star/static/media/logo.5d5d9eef.svg"
+  }
+]);
